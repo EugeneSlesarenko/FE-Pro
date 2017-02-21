7e588e05a8af9e4334a1f8faeba6a924224dfb65
Eugene Slesarenko <eu.slesarenko@yahoo.com>
2017-02-21 19:04:16 +0200

    HW5

---
+++ L5/Slesarenko.js
@@ -0,0 +1,46 @@
+(function(){
+
+  function Circle(){
+
+    var top, left, width, height, backgroundColor;
+
+    setCoordinates = function( newTop, newLeft ){
+      top = newTop+'px';
+      left = newLeft+'px';
+    }
+
+    setSize = function(){
+      if (newWidth.value >= 600) {
+        alert('NO!');
+        newWidth.value = 0;
+        width = newWidth.value+ 'px';
+      }else {
+        width = newWidth.value+ 'px';
+      }
+      height = width;
+    }
+
+    setColor = function() {
+      backgroundColor = newColor.value;
+    }
+
+    this.create = function( event ){
+      var element = document.createElement('div'); //создали элемент
+      element.className = 'circle';
+      setCoordinates( event.layerY, event.layerX );
+      setSize();
+      setColor();
+      element.style.top = top;
+      element.style.left = left;
+      element.style.width = width;
+      element.style.height = height;
+      element.style.backgroundColor = backgroundColor;
+      area.appendChild(element); // добавить в area созданный лемент
+    }
+
+  }
+
+  var c = new Circle();
+  area.addEventListener( 'click', c.create )
+
+}())
